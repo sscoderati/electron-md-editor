7078dff651772c143c46178c552cd352f42772f6
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-04-16 21:47:45 +0900

    Chore: electron-vite config 설정 추가

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,20 +1,33 @@
-import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
+    resolve: {
+      alias: {
+        "@/lib": resolve("src/main/lib"),
+        "@shared": resolve("src/shared")
+      }
+    }
   },
   preload: {
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
+    assetsInclude: "src/renderer/assets/**",
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        "@renderer": resolve("src/renderer/src"),
+        "@shared": resolve("src/shared"),
+        "@/assets": resolve("src/renderer/src/assets"),
+        "@/components": resolve("src/renderer/src/components"),
+        "@/hooks": resolve("src/renderer/src/hooks"),
+        "@/store": resolve("src/renderer/src/store"),
+        "@/mocks": resolve("src/renderer/src/mocks")
       }
     },
     plugins: [react()]
   }
-})
+});
