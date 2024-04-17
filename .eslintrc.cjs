bc71ead3c20764cb3dff642d4f39a0fa2c65ad47
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-04-17 23:18:04 +0900

    Fix: eslintrc 설정 적용 오류 수정 외 1건
    - 페이지 컴포넌트 반환값 타입 제거

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,7 @@ module.exports = {
   ],
   rules: {
     "curly": "error",
-    "typescript-eslint/no-unused-vars": "off",
-    "typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
   }
 }
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,4 @@
-function App(): JSX.Element {
+function App() {
   return (
     <div className={"flex h-full items-center justify-center"}>
       <span className={"text-sky-400 font-bold text-4xl"}>Hello from Electron</span>
